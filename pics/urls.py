32bd82406f1992b67f2a4c34d1bb9f766b987791
Peter Flanner <peter.flanner@gmail.com>
2020-10-14 10:57:46 -0700

    Make logout page use logout template

--- pics/urls.py
+++ pics/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('users/<int:user_id>/follows/', views.user_follows, name='user_follows'),
     path('photos/<int:user_id>/', views.photos, name='photos'),
     path('media/<str:media_id>/', views.media, name='media'),
-    path('login/', auth_views.LoginView.as_view(template_name="pics/login.html", redirect_authenticated_user=True), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='pics/login.html', redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='pics/logout.html'), name='logout'),
 ]
 
