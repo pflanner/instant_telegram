5dc5fef8f2fe49d2b7f6af57f1b261b78bb9a3dc
Peter Flanner <peter.flanner@gmail.com>
2020-10-20 10:51:54 -0700

    Unlike view and url

--- pics/urls.py
+++ pics/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('follow/<int:user_id>/', views.follow, name='follow'),
     path('unfollow/<int:user_id>/', views.unfollow, name='unfollow'),
     path('like/<int:photo_id>', views.like, name='like'),
+    path('unlike/<int:photo_id>', views.unlike, name='unlike'),
     path('login/', auth_views.LoginView.as_view(template_name='pics/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='pics/logout.html'), name='logout'),
     path('signup/', views.signup, name='signup')
