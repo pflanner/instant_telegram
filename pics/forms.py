fdf05bf322058056714824f8aa2823cb25aeb527
Peter Flanner <peter.flanner@gmail.com>
2020-10-24 13:31:28 -0700

    Styling of login and sign up pages; add create an account link to login page

--- pics/forms.py
+++ pics/forms.py
@@ -1,12 +1,34 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
 
 class SignUpForm(UserCreationForm):
+    username = UsernameField()
     first_name = forms.CharField(max_length=150, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=150, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required.')
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
+    username.widget.attrs.update({'class': 'form-control'})
+    first_name.widget.attrs.update({'class': 'form-control'})
+    last_name.widget.attrs.update({'class': 'form-control'})
+    email.widget.attrs.update({'class': 'form-control'})
+    password1.widget.attrs.update({'class': 'form-control'})
+    password2.widget.attrs.update({'class': 'form-control'})
 
     class Meta:
         model = User
