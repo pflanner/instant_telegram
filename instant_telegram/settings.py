2ff45b9482224068716754261e78289b37c9631d
Peter Flanner <peter.flanner@gmail.com>
2020-10-28 12:35:44 -0700

    Add static root to settings

--- instant_telegram/settings.py
+++ instant_telegram/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# TODO change this for prod
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'secret key!'
 
@@ -141,5 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 S3_BUCKET = os.getenv('S3_BUCKET', '')
